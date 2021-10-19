42842ac39235f0f120f413d9a3b05ac5eac73c06
Khvostov_Nik <apuopx@gmail.com>
2021-10-19 11:17:28 +0300

    dialogs

--- src/Component/Dialogs/Dialogs.jsx
+++ src/Component/Dialogs/Dialogs.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
-// import s from './Dialogs.module.css';
+import React from "react";
+import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
-    return (
-        <div>
-            Dialogs
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogs_items}>
+        <div className={s.dialog + ' '+ s.active}>Kolayn</div>
+        <div className={s.dialog}>Dimych</div>
+        <div className={s.dialog}>Ivan</div>
+        <div className={s.dialog}>Valera</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Necessitatibus, deserunt.
         </div>
-    );
-}
+        <div className={s.message}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Necessitatibus, deserunt.
+        </div>
+        <div className={s.message}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Necessitatibus, deserunt.
+        </div>
+        <div className={s.message}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Necessitatibus, deserunt.
+        </div>
+        <div className={s.message}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Necessitatibus, deserunt.
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Dialogs;
