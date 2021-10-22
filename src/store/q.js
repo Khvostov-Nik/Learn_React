80a6fa83bf5686ee030d22d966d0264b488ec46d
Khvostov-Nik <apuopx@gmail.com>
2021-10-22 20:11:04 +0300

    создан файл q для другого проекта

---
+++ src/store/q.js
@@ -0,0 +1,29 @@
+export const store ={
+    testName:"Test_tb",
+    questions:[
+        {   
+            id:1,
+            question:"sdjfldjlgjhs",
+            answers:[
+                {answer:"dffsfsfd", flag:"true"},
+                {answer:"dffsfscvcvfd"},
+                {answer:"dffsfscvcvfdbcbccb"}
+        ]},
+        {   
+            id:2,
+            question:"sdjfldjlgjhs",
+            answers:[
+                {answer:"dffsfsfd", flag:"true"},
+                {answer:"dffsfscvcvfd"},
+                {answer:"dffsfscvcvfdbcbccb"}
+        ]},
+        {   
+            id:3,
+            question:"sdjfldjlgjhs",
+            answers:[
+                {answer:"dffsfsfd", flag:"true"},
+                {answer:"dffsfscvcvfd"},
+                {answer:"dffsfscvcvfdbcbccb"}
+        ]}
+    ]  
+}
