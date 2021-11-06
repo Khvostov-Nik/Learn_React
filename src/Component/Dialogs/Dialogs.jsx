0f9eaa99dea60cc116dfb6395a11a0388636369e
Khvostov-Nik <apuopx@gmail.com>
2021-11-06 18:46:35 +0300

    рефакторинг диалогов

--- src/Component/Dialogs/Dialogs.jsx
+++ src/Component/Dialogs/Dialogs.jsx
@@ -9,7 +9,13 @@ const Dialogs = (props) => {
   });
 
   let messagesElements = props.dialogPage.messages.map((message) => {
-    return <Messages messageId={message.id} message={message.message} key ={message.id} />;
+    return (
+      <Messages
+        messageId={message.id}
+        message={message.message}
+        key={message.id}
+      />
+    );
   });
 
   let addMessage = () => {
@@ -25,18 +31,18 @@ const Dialogs = (props) => {
     <div className={s.dialogs}>
       <div className={s.dialogs_items}>{dialogsElements}</div>
       <div className={s.messages}>
+        <div className={s.wrap_mesages}> {messagesElements}</div>
         <div className={s.addMessage__wrap}>
           <textarea
             className={s.add_message__area}
             onChange={onMessageChang}
             value={props.dialogPage.newMessageText}
-            placeholder="Текст нового"
+            placeholder=" Текст нового"
           />
-          <button type='btn' className={s.add_message} onClick={addMessage}>
+          <button type="btn" className={s.add_message} onClick={addMessage}>
             Add mesage
           </button>
-        </div>
-        <div> {messagesElements}</div>
+      </div>
       </div>
     </div>
   );
