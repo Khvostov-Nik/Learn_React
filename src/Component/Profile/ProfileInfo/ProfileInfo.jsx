59ec28da325a8d76483cd0b7bcec9110d089986d
Khvostov_Nik <apuopx@gmail.com>
2021-11-12 09:04:34 +0300

    f

--- src/Component/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Component/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import userPhoto from "./../../../img/avatar.png";
 import Preloader from "./../../common/preloader/Preloader";
 import s from "./ProfileInfo.module.css";
+
 const Profileinfo = (props) => {
   if (!props.profile) {
     return <Preloader />;
@@ -8,7 +10,15 @@ const Profileinfo = (props) => {
   return (
     <div className={s.profileinfo}>
       <div className={s.description_block}>
-        <img className={s.ava} src={props.profile.photos.large} alt="" />
+        <img
+          className={s.ava}
+          src={
+            props.profile.photos.large != null
+              ? props.profile.photos.large
+              : userPhoto
+          }
+          alt=""
+        />
         <div className={s.name}>{props.profile.fullName}</div>
         <div className={s.aboutMe}>{props.profile.aboutMe}</div>
       </div>
