b20a1964a141d61b7c8cb52966904db0ad04032d
Khvostov_Nik <apuopx@gmail.com>
2021-11-21 14:30:54 +0300

    добавили axios.create

--- src/api/api.js
+++ src/api/api.js
@@ -1,42 +1,27 @@
 import axios from "axios";
 
-const API_KEY = "f7c74fa8-78d2-415f-8e71-1b506f4aee99"
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  headers: { "API-KEY": "f7c74fa8-78d2-415f-8e71-1b506f4aee99" },
+});
 
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-  return axios.get(
-    `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-    { withCredentials: true }
-  ).then(response => {
+  return instance
+    .get(`users?page=${currentPage}&count=${pageSize}`)
+    .then((response) => {
       return response.data;
     });
 };
 
+export const follow = (userId) => {
+  return instance.post(`follow/${userId}`, {}).then((response) => {
+    return response.data;
+  });
+};
 
-export const follow = (userId) =>{
- return   axios.post(
-      `https://social-network.samuraijs.com/api/1.0/follow/${userId}`,
-      {},
-      {
-        withCredentials: true,
-        headers:{
-          "API-KEY": API_KEY
-        }
-      }
-    ).then(response => {
-        return response.data;
-      });
-}
-
-export const unFollow = (userId) =>{
-    return   axios.delete(
-         `https://social-network.samuraijs.com/api/1.0/follow/${userId}`,
-         {
-           withCredentials: true,
-           headers:{
-             "API-KEY": API_KEY
-           }
-         }
-       ).then(response => {
-           return response.data;
-         });
-   }
+export const unFollow = (userId) => {
+  return instance.delete(`follow/${userId}`).then((response) => {
+    return response.data;
+  });
+};
