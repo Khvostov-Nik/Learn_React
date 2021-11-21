7576512ba445a166ef1664d2e9650f8e5d5ec174
Khvostov_Nik <apuopx@gmail.com>
2021-11-21 13:37:24 +0300

    реализовали подписку на пользователей

--- src/Component/Users/Users.jsx
+++ src/Component/Users/Users.jsx
@@ -1,4 +1,5 @@
 import { Pagination } from "@mui/material";
+import axios from "axios";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import userPhoto from "./../../img/avatar.png";
@@ -42,7 +43,21 @@ const Users = (props) => {
                 <button
                   className={s.btn}
                   onClick={() => {
-                    props.unfollow(u.id);
+                    axios
+                      .delete(
+                        `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+                        {
+                          withCredentials: true,
+                          headers:{
+                            "API-KEY": "f7c74fa8-78d2-415f-8e71-1b506f4aee99"
+                          }
+                        }
+                      )
+                      .then((response) => {
+                        if (response.data.resultCode === 0) {
+                          props.unfollow(u.id);
+                        }
+                      });
                   }}
                 >
                   Unfollow
@@ -51,7 +66,22 @@ const Users = (props) => {
                 <button
                   className={s.btn}
                   onClick={() => {
-                    props.follow(u.id);
+                    axios
+                      .post(
+                        `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+                        {},
+                        {
+                          withCredentials: true,
+                          headers:{
+                            "API-KEY": "f7c74fa8-78d2-415f-8e71-1b506f4aee99"
+                          }
+                        }
+                      )
+                      .then((response) => {
+                        if (response.data.resultCode === 0) {
+                          props.follow(u.id);
+                        }
+                      });
                   }}
                 >
                   Follow
